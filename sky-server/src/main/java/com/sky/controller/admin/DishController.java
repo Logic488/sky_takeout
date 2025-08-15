2e5da998da81c832c6d243a4e3ae2ac0221fb379
achamster <innocentworks@outlook.com>
2025-08-16 00:04:51 +0800

    新增菜品，删除菜品，菜品分页查询业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,54 @@
+package com.sky.controller.admin;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/admin//dish")
+@Api(tags = "菜品相关接口")
+public class DishController {
+
+    @Autowired
+    private DishService dishService;
+
+    //新增菜品以及对应口味
+    @PostMapping
+    @ApiOperation("新增菜品")
+    public Result saveDish(@RequestBody DishDTO dishDTO){
+        log.info("新增菜品：{}", dishDTO);
+        dishService.saveWithFlavour(dishDTO);
+        return Result.success();
+    }
+
+    //菜品分页查询
+    @GetMapping("/page")
+    @ApiOperation("分页查询")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("分页查询菜品");
+        PageResult p = dishService.page(dishPageQueryDTO);
+        return Result.success(p);
+    }
+
+    //删除菜品
+    @DeleteMapping
+    @ApiOperation("删除菜品")
+    public Result deleteDish(@RequestParam List<Long> ids){
+        log.info("批量删除菜品：{}", ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
+    }
+
+
+}
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,22 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
+
+public interface DishService {
+
+    //新增菜品和对应的口味
+    public void saveWithFlavour(DishDTO dishDTO);
+
+    //分页查询菜品
+    PageResult page(DishPageQueryDTO dishPageQueryDTO);
+
+    //批量删除菜品
+    void deleteBatch(List<Long> ids);
+
+
+
+}
